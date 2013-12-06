cda384ed22ff143ee0cc9864ce8705155b8fc871
Ruzbeh Bacha <ruzbehbacha@gmail.com>
2013-12-06 13:28:33 +0000

    Blank rail project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pingpong::Application.config.secret_key_base = 'f4af050bfeacde6fa8c167b68418e96c320a6995816ca44f2a888f98c41e617a5a981b7ea2a481b4c8ec88a696cd4b12159129abc13f40f2f4d98e0441c4e3a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pingpong::Application.config.session_store :cookie_store, key: '_pingpong_session'
