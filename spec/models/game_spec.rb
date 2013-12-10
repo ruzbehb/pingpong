8a68d7b75e4cd0f4df287649b574e1ee3b11f9bd
Natalie Eskinazi <n.l.eskinazi@gmail.com>
2013-12-10 12:09:44 +0000

    Added more tests and methods for game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Game do
 	let(:score) { mock_model('Score') }
-	let(:game) { Game.new }
+	let(:game) { Game.new(score: score) }
 
 	context 'keeping track of scores' do
 
@@ -17,19 +17,39 @@ describe Game do
 	end
 
 	it "knows when it's ended" do
+		allow(score).to receive(:game_won)
+
 		11.times { game.award_point }
 		expect(game.completed?).to be_true
 	end
 
 	it "must have a number assigned to it" do
 		game.number = nil
-    game.should have(1).error_on(:number)
+    expect(game).to have(1).error_on(:number)
     game.number = 1
-    game.should have(:no).errors_on(:number)
+    expect(game).to have(:no).errors_on(:number)
 	end
 
 	it 'notifies the Score when the game finishes' do
-		game = Game.new(score: score)
+		expect(score).to receive(:game_won)
+
+		11.times { game.award_point }
 	end
+
+	it "can delete a point" do
+		game.points = 9
+		game.award_point
+		expect(game.points).to eq 10
+		game.delete_point
+		expect(game.points).to eq 9
+	end
+
+	it "does nothing if deleting a point at 0" do
+		game.points = 0
+		game.delete_point
+		expect(game.points).to eq 0
+	end
+
+
 
 end
