cfb8bb398e97a0bdf04f0b40245b01a3b09d8dca
Ruzbeh Bacha <ruzbehbacha@gmail.com>
2013-12-10 09:42:44 +0000

    added some tests for player profile

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,9 +1,20 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :player do
+  factory :player1 do
   	email 'helloworld@gmail.com'
   	password '12345678'
   	password_confirmation '12345678'
+  	provider 'facebook'
+  	uid '509597657'
+  	name 'Ruzbeh Bacha-ta'
+  	first_name 'Ruzbeh'
+  	last_name 'Bacha'
+  	image_url 'http://graph.facebook.com/509597657/picture?type=square'
+  	current_location 'London, United Kingdom'
+  	hometown 'Mumbai, India'
+  	fb_url 'https://www.facebook.com/ruzbehb'
+  	sex 'male'
+  	google_url 'https://plus.google.com/106017922227865222929'
   end
 end
--- spec/features/players_listing_and_details_spec.rb
+++ spec/features/players_listing_and_details_spec.rb
@@ -4,8 +4,16 @@ describe 'listing all players in the database' do
 
 	context 'user logged in' do
 
+		before do
+			login_as FactoryGirl.create(:player1)
+		end
+
 		it 'will show users own profile on top of the page' do
 
+
+		end
+
+		it 'will list all the players in the database' do
 
 		end
 
