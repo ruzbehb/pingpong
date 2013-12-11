97e0c4789266f600d99d069b0d0dca247aef3951
Ruzbeh Bacha <ruzbehbacha@gmail.com>
2013-12-11 09:47:21 +0000

    Added the FB like button

---
+++ app/assets/javascripts/facebook_like.js
@@ -0,0 +1,8 @@
+// document.write("<div id="fb-root"></div>");
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=219021668280216";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
