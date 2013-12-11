232690de5a0d1d46566cca185cd2f3ccd6ef3592
Will Bell <willbell@gmail.com>
2013-12-11 17:11:46 +0000

    Added simple form for new match

--- spec/features/match_feature_spec.rb
+++ spec/features/match_feature_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 
 describe 'starting a match' do
   
-  context 'guest starting match' do
+  context 'starting match' do
     it 'should let you enter your name' do
       visit new_match_path
       expect(page).to have_content 'Player 1 name:'
     end
+
+    it 'should show your name on scorer' do
+    	visit new_match_path
+    	fill_in 'Player 1 name:', with: 'Natalie'
+    	fill_in 'Player 2 name:', with: 'Will'
+    	click_button 'Start match!'
+    	expect(page).to have_content 'Natalie'
+    end
+
   end
 end
