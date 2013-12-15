f59ac63cbb15273c56ddceb5add0fec029917881
Will Bell <willbell@gmail.com>
2013-12-15 13:25:11 +0000

    Mid-refactoring of linechart.js

--- app/assets/javascripts/linechart.js
+++ app/assets/javascripts/linechart.js
@@ -4,32 +4,34 @@ App.controller('LineChartController', function($scope){
   
   $scope.data = [];
 
+  var playerRecords = [{points: 0, coordinates: [{x:0,y:115}]},
+                        {points: 0, coordinates: [{x:0,y:115}]}]
 
-  var players = [{points: 0, coordinates: [{x:0,y:115}]},
-                 {points: 0, coordinates: [{x:0,y:115}]}]
-
-  var addCoordinates = function(player, i){
-    player.coordinates.push({x: (i+1)*10, y: 115-player.points*10});
+  var addCoordinates = function(playerIndex, nthRally){
+    playerIndex.coordinates.push({x: (nthRally+1)*10, y: 115-playerIndex.points*10});
   }
 
-  var addWinnerI = function(winner,i){
-    players[winner].points +=1;
-    addCoordinates(players[0], i);
-    addCoordinates(players[1], i);
+  var addRally = function(winnerIndex,nthRally){
+    playerRecords[winnerIndex].points +=1;
+    addCoordinates(playerRecords[0], nthRally);
+    addCoordinates(playerRecords[1], nthRally);
   }
-  angular.forEach($scope.data, addWinnerI);
 
-  $scope.addWinner = function(winner){
-    $scope.data.push(winner);
-    addWinnerI(winner, $scope.data.length-1);
+  // angular.forEach($scope.data, addRally);
+
+  $scope.addWinner = function(winnerIndex){
+    $scope.data.push(winnerIndex);
+    addRally(winnerIndex, $scope.data.length-1);
   }
-  $scope.scale = 10;
+
+  // $scope.scale = 10;
+
   var linePaths= function(player_index){
     return function() {
       var pathParts = [], currentPoint, i;
       
-      for (i = 0; i < players[player_index].coordinates.length; i++) {
-        currentPoint = players[player_index].coordinates[i];
+      for (i = 0; i < playerRecords[player_index].coordinates.length; i++) {
+        currentPoint = playerRecords[player_index].coordinates[i];
         pathParts.push(currentPoint.x + "," + currentPoint.y);
       }
 
@@ -47,8 +49,8 @@ App.controller('LineChartController', function($scope){
 //   p2_progress.push(new_point_p2);
 // }
 
-  $scope.points_1 = players[0].coordinates;
-  $scope.points_2 = players[1].coordinates;
+  $scope.points_1 = playerRecords[0].coordinates;
+  $scope.points_2 = playerRecords[1].coordinates;
   $scope.linePath1 = linePaths(0);
   $scope.linePath2 = linePaths(1);
   
